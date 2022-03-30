0f08fc1626395887b681718c222628886395772c
cbergz <charlie.bergman@gmail.com>
2022-03-30 14:19:15 -0400

    fix redirect to pubkey

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -9,7 +9,7 @@ const NavBar = () => {
   return (
     <div className="grid grid-cols-12 mb-3">
       <div className="col-span-12 xl:col-start-2 xl:col-span-10 flex h-20 items-center justify-between px-4 md:px-8 xl:px-4">
-        <Link href={fmtUrlWithCluster('/dao/ORCA')}>
+        <Link href={fmtUrlWithCluster('/dao/66Du7mXgS2KMQBUk6m9h3TszMjqZqdWhsG3Duuf69VNW')}>
           <div className="cursor-pointer flex items-center">
             <img src="/img/orca.svg" className="h-8 mr-3" />
             {/* <h1 className="text-lg">Sierra</h1> */}

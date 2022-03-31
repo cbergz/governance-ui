44439d11e1a21b1604409aa65e7db99eb0383755
cbergz <charlie.bergman@gmail.com>
2022-03-31 00:28:30 -0400

    vote with admin

--- pages/dao/[symbol]/proposal/components/VoteBySwitch.tsx
+++ pages/dao/[symbol]/proposal/components/VoteBySwitch.tsx
@@ -6,7 +6,7 @@ const VoteBySwitch = ({ checked, onChange }) => {
   const { toManyCouncilOutstandingProposalsForUse } = useRealm()
   return !toManyCouncilOutstandingProposalsForUse ? (
     <div className="text-sm mb-3">
-      <div className="mb-2">Vote by council</div>
+      <div className="mb-2">Vote with Admin tokens</div>
       <div className="flex flex-row text-xs items-center">
         <Switch checked={checked} onChange={onChange} />
       </div>

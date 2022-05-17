e37eb52fe43cd8171fa1a816c19574487a53c836
cbergz <charlie.bergman@gmail.com>
2022-05-17 10:47:17 -0400

    background to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
     purgeLayersByDefault: true,
   },
   theme: {
+    backgroundImage: {
+      orca: "url('background_orca.svg')",
+    },
     fontFamily: {
       display: ['PT Mono, monospace'],
       body: ['Inter, sans-serif'],
@@ -38,6 +41,23 @@ module.exports = {
           blue: '#8AACEB',
         },
         'light-theme': {
+          primary: { light: '#292929', dark: '#101010' },
+          'secondary-1': { light: '#AFD803', dark: '#6CBF00' },
+          'secondary-2': { light: '#E54033', dark: '#C7251A' },
+          'fgd-1': '#3d3d3d',
+          'fgd-2': '#7a7a7a',
+          'fgd-3': '#adadad',
+          'fgd-4': '#cccccc',
+          'bkg-1': '#fcfcfc',
+          'bkg-2': '#f0f0f0',
+          'bkg-3': '#e0e0e0',
+          'bkg-4': '#d6d6d6',
+          orange: '#F7A531',
+          red: '#cb676f',
+          green: '#6BBF5F',
+          blue: '#3F77DE',
+        },
+        'orca-theme': {
           primary: { light: '#292929', dark: '#101010' },
           'secondary-1': { light: '#AFD803', dark: '#6CBF00' },
           'secondary-2': { light: '#E54033', dark: '#C7251A' },

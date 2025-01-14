4a54f858b43000c5b08c27cc346991accf2fa5a5
cbergz <charlie.bergman@gmail.com>
2025-01-13 22:55:57 -0500

    revert rpc

--- constants/endpoints.ts
+++ constants/endpoints.ts
@@ -1,7 +1,7 @@
 export const MAINNET_RPC =
   process.env.NEXT_PUBLIC_MAINNET_RPC ||
   process.env.MAINNET_RPC ||
-  'http://realms-realms-c335.mainnet.rpcpool.com'
+  'https://mainnet.helius-rpc.com/?api-key=6b9cdf4b-db5c-498c-ba47-475472be9699'
 
 export const DEVNET_RPC =
   process.env.NEXT_PUBLIC_DEVNET_RPC ||

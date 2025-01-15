b562dafe2d178c5e5e3d17a42b874093d4d21bf7
cbergz <charlie.bergman@gmail.com>
2025-01-14 21:02:01 -0500

    try with quiknode

--- constants/endpoints.ts
+++ constants/endpoints.ts
@@ -1,7 +1,7 @@
 export const MAINNET_RPC =
   process.env.NEXT_PUBLIC_MAINNET_RPC ||
   process.env.MAINNET_RPC ||
-  'https://mainnet.helius-rpc.com/?api-key=6b9cdf4b-db5c-498c-ba47-475472be9699'
+  'https://sly-patient-waterfall.solana-mainnet.quiknode.pro/bceccc776283213e297966f41ab4fae12c966c7d'
 
 export const DEVNET_RPC =
   process.env.NEXT_PUBLIC_DEVNET_RPC ||
